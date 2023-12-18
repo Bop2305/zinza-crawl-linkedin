928cdf067aefbe4d740fbea1a9992356ad2cbbb0
ThongNC <thong.nc@zinza.com.vn>
2023-12-18 23:03:30 +0700

    type index job table

--- apis/src/job/job.entity.ts
+++ apis/src/job/job.entity.ts
@@ -1,35 +1,36 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
+@Index(['job_type', 'experience_level', 'required_skills'])
 export class JobDetail {
     @PrimaryGeneratedColumn('uuid')
     id: string
 
-    @Column({nullable: true})
+    @Column({ nullable: true })
     job_position: string
 
-    @Column({nullable: true})
+    @Column({ nullable: true })
     company_name: string
 
-    @Column({nullable: true})
+    @Column({ nullable: true })
     company_link: string
 
-    @Column({nullable: true})
+    @Column({ nullable: true })
     job_type: string
-    
-    @Column({nullable: true})
+
+    @Column({ nullable: true })
     work_schedule: string
 
-    @Column({nullable: true})
+    @Column({ nullable: true })
     experience_level: string
 
-    @Column({nullable: true})
+    @Column({ nullable: true })
     job_description: string
 
-    @Column({nullable: true})
+    @Column({ nullable: true })
     required_skills: string
 
-    @Column({nullable: true})
+    @Column({ nullable: true })
     linkedin_url: string
 
     @CreateDateColumn()
